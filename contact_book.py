22669408fe9e45dc884b5753a363d59f08c9c333
shamswardak <102758614+shamswardak@users.noreply.github.com>
2022-04-12 21:26:55 -0400

    Add entry boxes for each contact element

--- contact_book.py
+++ contact_book.py
@@ -11,4 +11,9 @@ Label(root, text = 'Last Name').place(x=5,y=25)
 Label(root, text= 'Phone').place(x=5,y=45)
 Label(root, text= 'Address').place(x=5,y=65)
 
+first_name_entry = Entry(root, width=20).place(x=80,y=5)
+last_name_entry = Entry(root, width=20).place(x=80,y=25)
+phone_entry = Entry(root, width=20).place(x=80,y=45)
+address_entry = Entry(root, width=20).place(x=80,y=65)
+
 root.mainloop()
