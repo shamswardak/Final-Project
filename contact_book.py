97532ce18b6a69f829735dbe8d2c14effbe12c81
shamswardak <102758614+shamswardak@users.noreply.github.com>
2022-04-12 21:20:36 -0400

    Add labels of a contact element

--- contact_book.py
+++ contact_book.py
@@ -6,5 +6,9 @@ root.title('Contact Book')
 root.geometry('700x700')
 root.resizable(0, 0)
 
+Label(root, text= 'First Name').place(x=5,y=5)
+Label(root, text = 'Last Name').place(x=5,y=25)
+Label(root, text= 'Phone').place(x=5,y=45)
+Label(root, text= 'Address').place(x=5,y=65)
 
 root.mainloop()
