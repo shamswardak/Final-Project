b0f185bedffe7b72fb96897b9a1c501d4fac2827
shamswardak <102758614+shamswardak@users.noreply.github.com>
2022-05-05 13:13:36 -0400

    delete full contact box when a contact is deleted

--- contact_book.py
+++ contact_book.py
@@ -113,6 +113,8 @@ def delete_contact():
     selected_phone_number = re.findall(r"\d{10}", selected) #Use a regular expression to identify the phone number from the full contact information and save it.
     database.delete_from_database(int(selected_phone_number[0]))
     info_box.delete(cursor_selected)
+    full_contact.delete(0, END)
+
 
 
 root = Tk() 
